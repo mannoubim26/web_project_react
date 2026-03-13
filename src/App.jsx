8cdeb2c7b02fd2db38359fb09ed6248f3de7ef5b
tameemraihan <tameem.raihan@gmail.com>
2026-03-13 12:13:23 +0100

    Delete Meal Plan and use json file instead

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { LanguageProvider } from './context/LanguageContext';
 import { ROUTES } from './constants';
 import Login from './screens/Login/Login';
 import Home from './screens/Home/Home';
-import MealPlan from './screens/MealPlan/MealPlan';
+import Meal from './screens/Meal/Meal';
 import Exercises from './screens/Exercises/Exercises';
 import Progress from './screens/Progress/Progress';
 import Profile from './screens/Profile/Profile';
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.MEALS} element={<MealPlan />} />
+          <Route path={ROUTES.MEALS} element={<Meal />} />
           <Route path={ROUTES.EXERCISES} element={<Exercises />} />
           <Route path={ROUTES.PROGRESS} element={<Progress />} />
           <Route path={ROUTES.PROFILE} element={<Profile />} />
